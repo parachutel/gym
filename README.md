864afd37ff1347313084f06ace9fa6d2cb01c9a0
shengli <lisheng@stanford.edu>
2019-08-28 17:25:36 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Modifications:
+- 08/28/19 Modified gym.envs.classic_control.rendering. Added Sprite and Text.
---
+++ wrappers/__init__.py
@@ -0,0 +1,7 @@
+from gym import error
+from gym.wrappers.monitor import Monitor
+from gym.wrappers.time_limit import TimeLimit
+from gym.wrappers.dict import FlattenDictWrapper
+from gym.wrappers.atari_preprocessing import AtariPreprocessing
+from gym.wrappers.clip_reward import ClipReward
+from gym.wrappers.sign_reward import SignReward
